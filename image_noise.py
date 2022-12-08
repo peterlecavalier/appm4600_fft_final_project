1d484115163057a2d83460611a1d94beb264b31a
Peter LeCavalier <pele6150@colorado.edu>
2022-12-08 14:59:42 -0700

    Fixed most font sizes

--- image_noise.py
+++ image_noise.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 # Read in our image and plot it
 img = plt.imread('aerial.jpg')
 plt.imshow(img)
-plt.title('Original image with periodic noise')
+plt.title('Original image with periodic noise', fontdict={'fontsize':18})
 plt.tight_layout()
 plt.savefig('noise_img.png')
 plt.show()
@@ -19,7 +19,7 @@ scaled_fft = 10*np.log10(np.abs(shift_fft))
 
 # Show the original fft
 plt.imshow(scaled_fft)
-plt.title('Original Power Spectrum')
+plt.title('Original Power Spectrum', fontdict={'fontsize':18})
 plt.tight_layout()
 plt.savefig('og_noise_spectrum.png')
 plt.show()
@@ -27,13 +27,13 @@ plt.show()
 # Show zoomed in power spectrum, and removed peaks
 fig, ax = plt.subplots(1, 3, figsize=(15, 5))
 ax[0].imshow(scaled_fft[290:520, 540:800])
-ax[0].set_title('Original Power Spectrum (zoomed)')
+ax[0].set_title('Original Power Spectrum (zoomed)', fontsize=18)
 ax[1].imshow(scaled_fft[290:520, 540:800] >= 69)
-ax[1].set_title('Periodic noise peaks')
+ax[1].set_title('Periodic noise peaks', fontsize=18)
 new_scaled_fft = np.copy(scaled_fft)[290:520, 540:800]
 new_scaled_fft[new_scaled_fft >= 69] = 0
 ax[2].imshow(new_scaled_fft)
-ax[2].set_title('Spectrum (zoomed) with peaks removed')
+ax[2].set_title('Spectrum with peaks removed', fontsize=18)
 plt.tight_layout()
 plt.savefig('noise_spectrums.png')
 plt.show()
@@ -47,7 +47,7 @@ inverse_img = np.fft.ifft2(inverse_scale)
 
 # Show the final image
 plt.imshow(np.real(inverse_img))
-plt.title('Image with periodic noise removed')
+plt.title('Image with periodic noise removed', fontdict={'fontsize':18})
 plt.tight_layout()
 plt.savefig('final_noise.png')
 plt.show()
